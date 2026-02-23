71ebe3b9e4d74f5ef26bdbc63db9f17f31655747
Chris <chriscamano007@gmail.com>
2026-02-22 20:14:39 -0800

    hotpatch4

--- _plugins/bibtex_ruby_compat.rb
+++ _plugins/bibtex_ruby_compat.rb
@@ -1,24 +1,24 @@
 # Ruby 3 compatibility patch for bibtex-ruby 4.4.x.
-# The gem uses Proc.new without an explicit block in a couple of iterator
-# methods, which raises ArgumentError on Ruby 3.
+# The gem uses Proc.new without an explicit block in iterator methods,
+# which raises ArgumentError on Ruby 3.
+
+begin
+  require 'bibtex'
+rescue LoadError
+  # If bibtex isn't available, let normal load fail later with a clear error.
+end
 
 if defined?(BibTeX::Bibliography)
   class BibTeX::Bibliography
     def each(&block)
-      if block
-        data.each(&block)
-        self
-      else
-        to_enum(:each)
-      end
+      return to_enum(:each) unless block
+      data.each(&block)
+      self
     end
 
     def each_entry(&block)
-      if block
-        q('@entry').each(&block)
-      else
-        q('@entry').to_enum
-      end
+      return q('@entry').to_enum unless block
+      q('@entry').each(&block)
     end
   end
 end
